077bdfda774d09d984cb14dedb529665f7a707e7
Daniel Bishop <dbishop@beachfleischman.com>
2025-07-28 13:18:08 -0700

    Bio tidy and smaller text in accordions

--- theme/functions.php
+++ theme/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Load Lifter K functions and definitions
  *
@@ -7,7 +8,7 @@
  * @package Load_Lifter_K
  */
 
-if ( ! defined( 'LL_VERSION' ) ) {
+if (! defined('LL_VERSION')) {
 	/*
 	 * Set the theme’s version number.
 	 *
@@ -15,17 +16,17 @@ if ( ! defined( 'LL_VERSION' ) ) {
 	 * to create your production build, the value below will be replaced in the
 	 * generated zip file with a timestamp, converted to base 36.
 	 */
-	define( 'LL_VERSION', '2.2.3' );
+	define('LL_VERSION', '2.2.4');
 }
 
-if ( ! defined( 'LL_COMPANY_LEGAL_NAME' ) ) {
-	define( 'LL_COMPANY_LEGAL_NAME', 'Kuadra Support' );
+if (! defined('LL_COMPANY_LEGAL_NAME')) {
+	define('LL_COMPANY_LEGAL_NAME', 'Kuadra Support');
 }
-if ( ! defined( 'LL_COMPANY_NICE_NAME' ) ) {
-	define( 'LL_COMPANY_NICE_NAME', 'Kuadra' );
+if (! defined('LL_COMPANY_NICE_NAME')) {
+	define('LL_COMPANY_NICE_NAME', 'Kuadra');
 }
 
-if ( ! defined( 'LL_TYPOGRAPHY_CLASSES' ) ) {
+if (! defined('LL_TYPOGRAPHY_CLASSES')) {
 	/*
 	 * Set Tailwind Typography classes for the front end, block editor and
 	 * classic editor using the constant below.
@@ -50,7 +51,7 @@ if ( ! defined( 'LL_TYPOGRAPHY_CLASSES' ) ) {
 	);
 }
 
-if ( ! defined( 'LL_LP_TEMPLATES' ) ) {
+if (! defined('LL_LP_TEMPLATES')) {
 	define(
 		'LL_LP_TEMPLATES',
 		[
@@ -59,7 +60,7 @@ if ( ! defined( 'LL_LP_TEMPLATES' ) ) {
 	);
 }
 
-if ( ! defined( 'LL_NAV_PRIMARY' ) ) {
+if (! defined('LL_NAV_PRIMARY')) {
 	define(
 		'LL_NAV_PRIMARY',
 		[
@@ -78,7 +79,7 @@ if ( ! defined( 'LL_NAV_PRIMARY' ) ) {
 				"label" => 'About Us',
 				"url" => '/about/',
 				"hasChildren" => false,
-					"submenuContent" => '[listmenu menu="About menu" /]',
+				"submenuContent" => '[listmenu menu="About menu" /]',
 			],
 			"careers" => [
 				"label" => 'Careers',
@@ -114,7 +115,7 @@ if ( ! defined( 'LL_NAV_PRIMARY' ) ) {
 // 	);
 // }
 
-if ( ! defined( 'LL_KUADRA_SERVICES' ) ) {
+if (! defined('LL_KUADRA_SERVICES')) {
 	define(
 		'LL_KUADRA_SERVICES',
 		[
@@ -147,7 +148,7 @@ if ( ! defined( 'LL_KUADRA_SERVICES' ) ) {
 }
 
 
-if ( ! function_exists( 'll_setup' ) ) :
+if (! function_exists('ll_setup')) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -155,19 +156,20 @@ if ( ! function_exists( 'll_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function ll_setup() {
+	function ll_setup()
+	{
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on Load Lifter K, use a find and replace
 		 * to change 'loadlifter' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'loadlifter', get_template_directory() . '/languages' );
+		load_theme_textdomain('loadlifter', get_template_directory() . '/languages');
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
-		add_theme_support( 'title-tag' );
-		add_theme_support( 'post-thumbnails' );
+		add_theme_support('automatic-feed-links');
+		add_theme_support('title-tag');
+		add_theme_support('post-thumbnails');
 		add_theme_support(
 			'html5',
 			array(
@@ -183,63 +185,64 @@ if ( ! function_exists( 'll_setup' ) ) :
 
 		register_nav_menus(
 			[
-				'll_menu_simple' => __( 'Simple', 'loadlifter' ),
-				'll_menu_services' => __( 'Services menu', 'loadlifter' ),
-				'll_menu_about' => __( 'About menu', 'loadlifter' ),
-				'll_menu_col_1' => __( 'Footer Left', 'loadlifter' ),
-				'll_menu_below_disclaimers' => __( 'Footer Below Disclaimers', 'loadlifter' ),
+				'll_menu_simple' => __('Simple', 'loadlifter'),
+				'll_menu_services' => __('Services menu', 'loadlifter'),
+				'll_menu_about' => __('About menu', 'loadlifter'),
+				'll_menu_col_1' => __('Footer Left', 'loadlifter'),
+				'll_menu_below_disclaimers' => __('Footer Below Disclaimers', 'loadlifter'),
 			]
 		);
 
-		add_theme_support( 'responsive-embeds' );
-		add_theme_support( 'disable-custom-font-sizes' );
+		add_theme_support('responsive-embeds');
+		add_theme_support('disable-custom-font-sizes');
 		// add_theme_support( 'customize-selective-refresh-widgets' );
-		add_theme_support( 'editor-styles' );
-		add_editor_style( 'style-editor.css' );
-		add_editor_style( 'style-editor-extra.css' );
+		add_theme_support('editor-styles');
+		add_editor_style('style-editor.css');
+		add_editor_style('style-editor-extra.css');
 
-		add_filter( 'feed_links_show_comments_feed', '__return_false' );
+		add_filter('feed_links_show_comments_feed', '__return_false');
 
-		remove_theme_support( 'block-templates' );
-		remove_action( 'wp_head', 'rsd_link' );
-		remove_action( 'wp_head', 'wp_generator' );
-		remove_action( 'wp_head', 'index_rel_link' );
-		remove_action( 'wp_head', 'wlwmanifest_link' );
-		remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
-		remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
-		remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
-		remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
-		remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
-		remove_action( 'wp_head', 'print_emoji_detection_script', 7);
-		remove_action( 'wp_print_styles', 'print_emoji_styles');
-		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-		remove_action( 'admin_print_styles', 'print_emoji_styles' );
+		remove_theme_support('block-templates');
+		remove_action('wp_head', 'rsd_link');
+		remove_action('wp_head', 'wp_generator');
+		remove_action('wp_head', 'index_rel_link');
+		remove_action('wp_head', 'wlwmanifest_link');
+		remove_action('wp_head', 'start_post_rel_link', 10, 0);
+		remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+		remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+		remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+		remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+		remove_action('wp_head', 'print_emoji_detection_script', 7);
+		remove_action('wp_print_styles', 'print_emoji_styles');
+		remove_action('admin_print_scripts', 'print_emoji_detection_script');
+		remove_action('admin_print_styles', 'print_emoji_styles');
 	}
 endif;
-add_action( 'after_setup_theme', 'll_setup' );
+add_action('after_setup_theme', 'll_setup');
 
 
 /**
  * Enqueue scripts and styles.
  */
-function ll_scripts() {
+function ll_scripts()
+{
 	// wp_register_style( 'a11y-slider-base', 'https://unpkg.com/a11y-slider@latest/dist/a11y-slider.css', [], '' );
-	wp_enqueue_style( 'loadlifter-style', get_stylesheet_uri(), array(), LL_VERSION );
-	if ( get_field( 'll_postpage_css' ) ) {
-		$inline_css = get_field( 'll_postpage_css' );
-		wp_add_inline_style( 'loadlifter-style', $inline_css );
+	wp_enqueue_style('loadlifter-style', get_stylesheet_uri(), array(), LL_VERSION);
+	if (get_field('ll_postpage_css')) {
+		$inline_css = get_field('ll_postpage_css');
+		wp_add_inline_style('loadlifter-style', $inline_css);
 	}
 
 	// wp_register_script( 'a11y-slider', 'https://unpkg.com/a11y-slider@latest/dist/a11y-slider.js', [], '', false );
-	wp_register_script( 'a11y-slider', get_template_directory_uri() . '/js/a11y-slider.min.js', [], LL_VERSION, false );
-	wp_register_script( 'block-litevimeoembed', 'https://cdn.jsdelivr.net/npm/lite-vimeo-embed/+esm', [], false, false );
-	wp_enqueue_script( 'fa-kit', 'https://kit.fontawesome.com/a2c7d80c94.js' );
+	wp_register_script('a11y-slider', get_template_directory_uri() . '/js/a11y-slider.min.js', [], LL_VERSION, false);
+	wp_register_script('block-litevimeoembed', 'https://cdn.jsdelivr.net/npm/lite-vimeo-embed/+esm', [], false, false);
+	wp_enqueue_script('fa-kit', 'https://kit.fontawesome.com/a2c7d80c94.js');
 
-	if ( !is_page_template( LL_LP_TEMPLATES ) ) {
-		wp_enqueue_script( 'loadlifter-script', get_template_directory_uri() . '/js/script.min.js', [ 'wp-blocks' ], LL_VERSION, true );
+	if (!is_page_template(LL_LP_TEMPLATES)) {
+		wp_enqueue_script('loadlifter-script', get_template_directory_uri() . '/js/script.min.js', ['wp-blocks'], LL_VERSION, true);
 	}
 }
-add_action( 'wp_enqueue_scripts', 'll_scripts' );
+add_action('wp_enqueue_scripts', 'll_scripts');
 
 
 /**
@@ -255,41 +258,43 @@ add_action( 'wp_enqueue_scripts', 'll_scripts' );
 // 	global $wp_scripts;
 // 	echo "\n";
 
-	// echo '<link rel="preload" as="image" href="/wp-content/themes/loadlifter/img/phx-desert-color-no-crop-small.jpg" media="(max-width: 767px)" />
-	// <link rel="preload" as="image" href="/wp-content/themes/loadlifter/img/phx-desert-color-no-crop.jpg" media="(min-width: 768px)" />';
-	// echo '<link rel="preload" as="script" href="https://js.hsforms.net/forms/v2.js?ver=' . wp_get_theme()->get('Version') . '" />';
+// echo '<link rel="preload" as="image" href="/wp-content/themes/loadlifter/img/phx-desert-color-no-crop-small.jpg" media="(max-width: 767px)" />
+// <link rel="preload" as="image" href="/wp-content/themes/loadlifter/img/phx-desert-color-no-crop.jpg" media="(min-width: 768px)" />';
+// echo '<link rel="preload" as="script" href="https://js.hsforms.net/forms/v2.js?ver=' . wp_get_theme()->get('Version') . '" />';
 
-	// foreach ($wp_scripts->queue as $handle) {
-	// 	$script = $wp_scripts->registered[$handle];
+// foreach ($wp_scripts->queue as $handle) {
+// 	$script = $wp_scripts->registered[$handle];
 
-	// 	//-- If version is set, append to end of source.
-	// 	$source = $script->src . ($script->ver ? "?ver={$script->ver}" : "");
+// 	//-- If version is set, append to end of source.
+// 	$source = $script->src . ($script->ver ? "?ver={$script->ver}" : "");
 
-	// 	//-- Spit out the tag.
-	// 	echo "<link rel='preload' href='{$source}' as='script'/>\n";
-	// }
+// 	//-- Spit out the tag.
+// 	echo "<link rel='preload' href='{$source}' as='script'/>\n";
+// }
 
 // 	echo '<link rel="preload" href="' . get_stylesheet_directory_uri() . '/fonts/serenity-light-webfont.woff2" as="font" type="font/woff2">' . "\r\n";
 // 	echo '<link rel="preload" href="' . get_stylesheet_directory_uri() . '/fonts/serenity-bold-webfont.woff2" as="font" type="font/woff2">' . "\r\n";
 // }
 
 
-add_filter( 'flying_press_exclude_from_minify:css' , function ($exclude_keywords) {
+add_filter('flying_press_exclude_from_minify:css', function ($exclude_keywords) {
 	$exclude_keywords = ['loadlifter'];
 	return $exclude_keywords;
 });
 
 
-function ll_disable_wp_links_menu() {
-	remove_menu_page( 'link-manager.php' );
+function ll_disable_wp_links_menu()
+{
+	remove_menu_page('link-manager.php');
 }
 
 
-function ll_load_admin_styles() {
-	wp_register_style( 'rsms-inter', 'https://rsms.me/inter/inter.css' );
-	wp_enqueue_style( 'rsms-inter' );
+function ll_load_admin_styles()
+{
+	wp_register_style('rsms-inter', 'https://rsms.me/inter/inter.css');
+	wp_enqueue_style('rsms-inter');
 
-	wp_enqueue_style( 'll-admin', get_template_directory_uri().'/admin.css' );
+	wp_enqueue_style('ll-admin', get_template_directory_uri() . '/admin.css');
 }
 
 
@@ -303,21 +308,21 @@ function ll_load_admin_styles() {
 // ^^^ If they ever add blog posts and need related posts, install YARPP and uncomment these lines
 
 
-switch( wp_get_environment_type() ) {
+switch (wp_get_environment_type()) {
 	case 'local':
-		add_action( 'admin_enqueue_scripts', 'll_load_admin_styles' );
+		add_action('admin_enqueue_scripts', 'll_load_admin_styles');
 		// add_action( 'wp_head', 'll_preload_assets', -1 );
 		break;
 
 	case 'staging':
-		add_action( 'admin_enqueue_scripts', 'll_load_admin_styles' );
+		add_action('admin_enqueue_scripts', 'll_load_admin_styles');
 		break;
 
 	default:
-		add_action( 'admin_menu', 'll_disable_wp_links_menu' );
-		add_action( 'admin_enqueue_scripts', 'll_load_admin_styles' );
+		add_action('admin_menu', 'll_disable_wp_links_menu');
+		add_action('admin_enqueue_scripts', 'll_load_admin_styles');
 		/* Hide Jetpack upsell ads */
-		add_filter( 'jetpack_just_in_time_msgs', '__return_false', 99 );
+		add_filter('jetpack_just_in_time_msgs', '__return_false', 99);
 		break;
 }
 
@@ -325,20 +330,22 @@ switch( wp_get_environment_type() ) {
 /**
  * Enqueue CSS and JS for A11y Slider when shortcode is present
  */
-function ll_enq_a11y_slider_assets() {
+function ll_enq_a11y_slider_assets()
+{
 	global $post;
-	if( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'a11yslider' ) ) {
+	if (is_a($post, 'WP_Post') && has_shortcode($post->post_content, 'a11yslider')) {
 		// wp_enqueue_style( 'a11y-slider-base' );
-		wp_enqueue_script( 'a11y-slider' );
+		wp_enqueue_script('a11y-slider');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'll_enq_a11y_slider_assets' );
+add_action('wp_enqueue_scripts', 'll_enq_a11y_slider_assets');
 
 /**
  * Enqueue the block editor script.
  */
-function ll_enqueue_block_editor_script() {
-	if ( is_admin() ) {
+function ll_enqueue_block_editor_script()
+{
+	if (is_admin()) {
 		wp_enqueue_script(
 			'loadlifter-editor',
 			get_template_directory_uri() . '/js/block-editor.min.js',
@@ -349,10 +356,10 @@ function ll_enqueue_block_editor_script() {
 			LL_VERSION,
 			true
 		);
-		wp_add_inline_script( 'loadlifter-editor', "tailwindTypographyClasses = '" . esc_attr( LL_TYPOGRAPHY_CLASSES ) . "'.split(' ');", 'before' );
+		wp_add_inline_script('loadlifter-editor', "tailwindTypographyClasses = '" . esc_attr(LL_TYPOGRAPHY_CLASSES) . "'.split(' ');", 'before');
 	}
 }
-add_action( 'enqueue_block_assets', 'll_enqueue_block_editor_script' );
+add_action('enqueue_block_assets', 'll_enqueue_block_editor_script');
 
 /**
  * Add the Tailwind Typography classes to TinyMCE.
@@ -360,11 +367,12 @@ add_action( 'enqueue_block_assets', 'll_enqueue_block_editor_script' );
  * @param array $settings TinyMCE settings.
  * @return array
  */
-function ll_tinymce_add_class( $settings ) {
+function ll_tinymce_add_class($settings)
+{
 	$settings['body_class'] = LL_TYPOGRAPHY_CLASSES;
 	return $settings;
 }
-add_filter( 'tiny_mce_before_init', 'll_tinymce_add_class' );
+add_filter('tiny_mce_before_init', 'll_tinymce_add_class');
 
 
 /**
@@ -388,7 +396,7 @@ require get_template_directory() . '/inc/cpt-testimonials.php';
 /**
  * Register block categories, patterns, and styles
  */
-remove_theme_support( 'core-block-patterns' );
+remove_theme_support('core-block-patterns');
 require get_template_directory() . '/inc/block-categories.php';
 require get_template_directory() . '/inc/block-styles.php';
 require get_template_directory() . '/inc/blocks.php';
--- theme/template-parts/content/content-people.php
+++ theme/template-parts/content/content-people.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying a Person (single view)
  *
@@ -8,45 +9,47 @@
  */
 
 $peepsep                        = ' <span class="sep opacity-60">|</span> ';
-$peepauthor                     = get_field( 'll_people_user' );
-$peep_thumbnail                 = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
-if ( $peep_thumbnail ) {
-	$headshot                   = esc_url( $peep_thumbnail[0] );
+$peepauthor                     = get_field('ll_people_user');
+$peep_thumbnail                 = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full');
+if ($peep_thumbnail) {
+	$headshot                   = esc_url($peep_thumbnail[0]);
 } else {
-	$headshot                   = esc_url( get_template_directory_uri() . '/img/headshot__empty.svg' );
+	$headshot                   = esc_url(get_template_directory_uri() . '/img/headshot__empty.svg');
 }
 
-if ( get_field( 'll_people_organization' ) === 'Kuadra Support' ) {
+if (get_field('ll_people_organization') === 'Kuadra Support') {
 	$peep_class                 = 'internal';
 } else {
 	$peep_class                 = 'external';
 }
 
-$peep_level                     = get_field( 'll_people_level' );
+$peep_level                     = get_field('ll_people_level');
 
-if ( $peepauthor ) {
+if ($peepauthor) {
 	$peepid                     = $peepauthor['ID'];
 	$peepname                   = $peepauthor['display_name'];
 	$peepnicename               = $peepauthor['user_nicename'];
 	// $peepfirstname           = $peepauthor['user_firstname'];
-	$person_archivelink         = sprintf( '<a href="/author/%1$s/">%2$s</a>', $peepnicename, $peepname );
-	$peeppostcount              = ( $peepauthor ) ? count_user_posts( $peepid, 'post', true ) : 0;
-	$recent_year_barrier        = date( "Y", strtotime( "-1 year" ) );
+	$person_archivelink         = sprintf('<a href="/author/%1$s/">%2$s</a>', $peepnicename, $peepname);
+	$peeppostcount              = ($peepauthor) ? count_user_posts($peepid, 'post', true) : 0;
+	$recent_year_barrier        = date("Y", strtotime("-1 year"));
 } else {
 	// $peepfirstname           = '';
 	$peeppostcount              = 0;
 }
 ?>
 
-<article <?php post_class( 'py-4 md:py-6 lg:py-8' ); ?>>
+<article <?php post_class('py-4 md:py-6 lg:py-8'); ?>>
 	<div class="px-2 container lg:px-[16px]">
 
-		<?php get_template_part( 'template-parts/layout/chunk', 'breadcrumbs' ); ?>
+		<?php get_template_part('template-parts/layout/chunk', 'breadcrumbs'); ?>
 
-		<div class="peepgrid peep-<?php echo $peep_class; ?> peep-<?php echo esc_attr( $peep_level['value'] ); ?> | md:grid md:grid-cols-3 gap-4 lg:grid-cols-4 lg:gap-16">
+		<div
+			class="peepgrid peep-<?php echo $peep_class; ?> peep-<?php echo esc_attr($peep_level['value']); ?> | md:grid md:grid-cols-3 gap-4 lg:grid-cols-4 lg:gap-16">
 
 			<div class="peepgrid-a | pb-8 md:pt-2 md:pb-0 md:order-2">
-				<div class="headshot-wrapper | relative before:content-[''] before:absolute before:top-2 before:left-2 before:w-full before:h-full before:bg-aqua-200 dark:before:bg-aqua-700 print:before:bg-none">
+				<div
+					class="headshot-wrapper | relative before:content-[''] before:absolute before:top-2 before:left-2 before:w-full before:h-full before:bg-aqua-200 dark:before:bg-aqua-700 print:before:bg-none">
 					<?php ll_people_headshot(); ?>
 				</div>
 			</div>
@@ -54,51 +57,51 @@ if ( $peepauthor ) {
 			<div class="peepgrid-b |  md:col-span-2 md:row-span-2 md:order-1 lg:col-span-3">
 				<header class="mb-4 flex flex-col items-center sm:flex-row sm:justify-between">
 					<div>
-					<?php
-					if ( $peep_class === 'internal' ) {
-						the_title( '<h1 class="entry-titl | mb-0 ">', '</h1>' );
+						<?php
+						if ($peep_class === 'internal') {
+							the_title('<h1 class="entry-titl | mb-0 ">', '</h1>');
 
-						if( get_field( 'll_people_designations' ) ) {
-							echo sprintf( '<h2 class="leading-normal tracking-tight text-aqua-700 dark:text-aqua-300">%1$s</h2>', get_field( 'll_people_designations' ) );
-						}
+							if (get_field('ll_people_designations')) {
+								echo sprintf('<h2 class="leading-normal tracking-tight text-aqua-700 dark:text-aqua-300">%1$s</h2>', get_field('ll_people_designations'));
+							}
 
-						if( get_field( 'll_people_title' ) ) {
-							echo sprintf( '<h2 class="text-neutral-700 dark:text-neutral-400">%1$s</h2>', get_field( 'll_people_title' ) );
-						}
-					} else {
-						the_title( '<h1 class="entry-title | mb-0 ">', '</h1>' );
+							if (get_field('ll_people_title')) {
+								echo sprintf('<h2 class="text-neutral-700 dark:text-neutral-400">%1$s</h2>', get_field('ll_people_title'));
+							}
+						} else {
+							the_title('<h1 class="entry-title | mb-0 ">', '</h1>');
 
-						if( get_field( 'll_people_title' ) ) {
-							echo sprintf( '<h2 class="text-neutral-900">%1$s</h2>', get_field( 'll_people_title' ) );
-						}
+							if (get_field('ll_people_title')) {
+								echo sprintf('<h2 class="text-neutral-900">%1$s</h2>', get_field('ll_people_title'));
+							}
 
-						if ( get_field( 'll_people_organization' ) ) {
-							echo sprintf( '<h2 class="text-neutral-600">%1$s</h2>', get_field( 'll_people_organization' ) );
+							if (get_field('ll_people_organization')) {
+								echo sprintf('<h2 class="text-neutral-600">%1$s</h2>', get_field('ll_people_organization'));
+							}
 						}
-					}
-					?>
+						?>
 					</div>
 					<div class="pl-2 md:pl-4">
-						<?php if ( get_field('ll_people_soc_linkedin') ) {
+						<?php if (get_field('ll_people_soc_linkedin')) {
 							echo '<a class="text-aqua-700 hover:text-aqua-500" href="' . get_field('ll_people_soc_linkedin') . '" aria-label="View LinkedIn profile"><i class="fa-brands fa-linkedin fa-2x"></i></a>';
 						} ?>
 					</div>
 				</header>
 
-				<div <?php ll_content_class( 'entry-content' ); ?>>
+				<div <?php ll_content_class('entry-content'); ?>>
 					<?php
 					the_content(
 						sprintf(
 							wp_kses(
 								/* translators: %s: Name of current post. Only visible to screen readers */
-								__( 'Continue reading<span> "%s"</span>', 'loadlifter' ),
+								__('Continue reading<span> "%s"</span>', 'loadlifter'),
 								array(
 									'span' => array(
 										'class' => array(),
 									),
 								)
 							),
-							wp_kses_post( get_the_title() )
+							wp_kses_post(get_the_title())
 						)
 					);
 					?>
@@ -110,7 +113,7 @@ if ( $peepauthor ) {
 
 			<aside class="peepgrid-c |  md:mt-0 md:order-3">
 
-				<?php get_template_part( 'template-parts/form/form-contact', 'sidebar' ); ?>
+				<?php get_template_part('template-parts/form/form-contact', 'sidebar'); ?>
 
 			</aside>
 		</div>
@@ -119,23 +122,30 @@ if ( $peepauthor ) {
 
 </article>
 
-<?php // if ( $peeppostcount > 0 ) : ?>
-	<!-- <section id="posts-by-<?php // the_ID(); ?>" <?php // post_class( 'bg-neutral-100 md:py-6 lg:py-8 dark:bg-neutral-950' ); ?>>
+<?php // if ( $peeppostcount > 0 ) : 
+?>
+<!-- <section id="posts-by-<?php // the_ID(); 
+														?>" <?php // post_class( 'bg-neutral-100 md:py-6 lg:py-8 dark:bg-neutral-950' ); 
+																									?>>
 		<div class="px-2 container lg:px-[16px]">
-			<h3 id="posts" class="mt-2 mb-4 text-4xl md:mb-8 text-brand-blue head-last-bold dark:text-neutral-300">Recent Insights by <strong><?php // echo $person_archivelink; ?></strong></h3>
-			<?php // echo do_shortcode( '[display-posts wrapper="ul" wrapper_class="dps-grid-4max cards-ic" layout="card-ic" author="'.$peepnicename.'" date_query_after="' .$recent_year_barrier. '-01-01" posts_per_page="3" orderby="modified"	no_posts_message="No recent posts found by this author." /]' ); ?>
+			<h3 id="posts" class="mt-2 mb-4 text-4xl md:mb-8 text-brand-blue head-last-bold dark:text-neutral-300">Recent Insights by <strong><?php // echo $person_archivelink; 
+																																																																				?></strong></h3>
+			<?php // echo do_shortcode( '[display-posts wrapper="ul" wrapper_class="dps-grid-4max cards-ic" layout="card-ic" author="'.$peepnicename.'" date_query_after="' .$recent_year_barrier. '-01-01" posts_per_page="3" orderby="modified"	no_posts_message="No recent posts found by this author." /]' ); 
+			?>
 		</div>
 	</section> -->
-<?php // endif; ?>
+<?php // endif; 
+?>
 
 <section class="full-bleed bg-aqua-50 ll-equal-vert-padding  |  dark:bg-atlantis-800 dark:text-neutral-200">
 	<div class="px-2 lg:px-[16px]">
 		<h3>On a personal note...</h3>
-		<?php if ( get_field( 'll_people_personal' ) ) { ?>
-			<blockquote class="peepquote | font-head italic text-xl text-aqua-800  |  dark:text-atlantis-200">&quot;<?php echo esc_html( get_field( 'll_people_personal' ) ); ?>&quot;</blockquote>
+		<?php if (get_field('ll_people_personal')) { ?>
+			<blockquote class="peepquote | font-head italic text-xl text-aqua-800  |  dark:text-atlantis-200">
+				&quot;<?php echo esc_html(get_field('ll_people_personal')); ?>&quot;</blockquote>
 		<?php } ?>
-		<p>&hellip;</p>
+		<!-- p>&hellip;</p>
 		<p>pictures?</p>
-		<p>Q&A?</p>
+		<p>Q&A?</p -->
 	</div>
 </section>
